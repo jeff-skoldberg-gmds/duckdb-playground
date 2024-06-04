5fd2edfc649fe7a3382a85262e3cc29e6359108b
jeff skoldberg <jeff.skoldberg@harvardbusiness.org>
2024-06-03 22:51:18 -0400

    setting up env vars, removing tracking on many files.

--- src/dbt-playing-with-duckdb/models/gmdata_s3/count_mrp_data.sql
+++ src/dbt-playing-with-duckdb/models/gmdata_s3/count_mrp_data.sql
@@ -1,4 +1,4 @@
-{{ config(materialized='external', location='src/dbt-playing-with-duckdb/models/gmdata_s3/myfile.csv') }}
+{{ config(materialized='external', location='models/gmdata_s3/myfile.csv') }}
 
 select count(*) total_row_count
 from 
