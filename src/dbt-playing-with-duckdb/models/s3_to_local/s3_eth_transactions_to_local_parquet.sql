4bc2ab9cbddc6b034c2efb0b37e6809b8e8df83e
Jeff Skoldberg <138161184+jeff-skoldberg-gmds@users.noreply.github.com>
2024-06-14 15:03:22 +0000

    updating

--- src/dbt-playing-with-duckdb/models/s3_to_local/s3_eth_transactions_to_local_parquet.sql
+++ src/dbt-playing-with-duckdb/models/s3_to_local/s3_eth_transactions_to_local_parquet.sql
@@ -1,9 +1,5 @@
-{#
-to write the model to S3 instead, just use:
-{{ config(materialized='external', location='s3://your/bucket/path/combined.parquet') }}
-and update profiles.yml to include it.
-#}
 
-{{ config(materialized='external', location='src/dbt-playing-with-duckdb/models/public_data/eth.parquet') }}
+
+{{ config(materialized='external', location='/workspaces/duckdb-playground/src/dbt-playing-with-duckdb/models/public_data/eth33.parquet') }}
 
 select * from {{ source('eth_s3', 'eth_transactions') }}
