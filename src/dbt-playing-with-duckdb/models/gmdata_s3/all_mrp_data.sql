6e354d305fc809a700330dc480be5460e842170b
Jeff Skoldberg <138161184+jeff-skoldberg-gmds@users.noreply.github.com>
2024-06-14 14:07:20 +0000

    renamed s3 to local

--- src/dbt-playing-with-duckdb/models/gmdata_s3/all_mrp_data.sql
+++
@@ -1,3 +0,0 @@
-{{ config(materialized='external', location='s3://gmds-sample-data/mrp_csv_data/combined.parquet') }}
-
-select * from {{ source('gmdata_s3', 'mrp_data') }}
--- src/dbt-playing-with-duckdb/models/gmdata_s3/count_mrp_data.sql
+++
@@ -1,5 +0,0 @@
-{{ config(materialized='external', location='models/gmdata_s3/myfile.csv') }}
-
-select count(*) total_row_count
-from 
-{{ ref('all_mrp_data') }}
